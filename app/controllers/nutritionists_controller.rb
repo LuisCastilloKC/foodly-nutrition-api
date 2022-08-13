beea913eccf7b299f01d5b2bf02f924e770d82b8
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-12 23:36:51 -0400

    define create method for nutritionist

--- app/controllers/nutritionists_controller.rb
+++ app/controllers/nutritionists_controller.rb
@@ -4,4 +4,15 @@ class NutritionistsController < ApplicationController
         nutritionist = Nutritionist.all
         render json: nutritionist
     end
+
+    def create 
+        nutritionist = Nutritionist.create(nutritionist_params)
+        if nutritionist.valid?
+            payload = {nutritionist_id: nutritionist.id}
+            token = encode_token(payload)
+            render json: {nutritionist: nutritionist, token: token}, status: :created
+        else
+            render: json: {errors: nutritionist.errors.full_messages}
+        end
+    end
 end
