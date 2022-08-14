2ae2eb8a7c06e8e3d5d6bf5c5cc0bb8ef6c99356
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-13 22:22:24 -0400

    add decode_token method and env secrect password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,15 @@ class ApplicationController < ActionController::API
         request.headers['Authorization']
     end
 
+    def decode_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, ENV['JWT_SECRET_PASSWORD'], true, algorithm: ENV['JWT_ALGORIGTH'])
+            rescue JWT::DecodeError
+                nill
+            end
+        end
+    end
+    
 end
