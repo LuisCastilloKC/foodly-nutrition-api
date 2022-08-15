2aae66fce85aca7191c0cb9b70679734c9b35f69
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-14 20:15:41 -0400

    fix syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :nutritionist, only:[:create, :index]
   post '/login', to: 'nutritionists#login'
-  get '/auto_login' to: 'nutritionists#auto_login'
+  get '/auto_login', to: 'nutritionists#auto_login'
 
 end
